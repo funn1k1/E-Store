4e4d52026e4412cd390219a9f7626c82335700a9
funn1k1 <yukovich01@mail.ru>
2021-03-22 02:51:27 +0300

    add UserRoles

---
+++ E-Store2021/ViewModels/UserRolesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Store2021.ViewModels
+{
+    public class UserRolesViewModel
+    {
+        public string UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
