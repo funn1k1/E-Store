e083e24668d8f005161e79dd882e1be79c3400ec
funn1k1 <yukovich01@mail.ru>
2021-03-26 04:12:29 +0300

    Fix: slick js and tabs

--- E-Store2021/wwwroot/js/main.js
+++ E-Store2021/wwwroot/js/main.js
@@ -2,8 +2,8 @@ $(function()
 {
     $('.banner-section_slider').slick({
         dots: true,
-        prevArrow: '<button class="banner-section_slider-btn banner-section_slider-btnprev"><img src="~/images/left-arrow.png" alt=""></button>',
-        nextArrow: '<button class="banner-section_slider-btn banner-section_slider-btnnext"><img src="~/images/right-arrow.png" alt=""></button>',
+        prevArrow: '<button class="banner-section_slider-btn banner-section_slider-btnprev"><img src="images/left-arrow.png" alt=""></button>',
+        nextArrow: '<button class="banner-section_slider-btn banner-section_slider-btnnext"><img src="images/right-arrow.png" alt=""></button>',
 
         responsive: [
             {
@@ -20,11 +20,12 @@ $(function()
         e.preventDefault();
 
         $($(this).siblings()).removeClass('tab--active');
-        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active')
+        $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active')
 
         $(this).addClass('tab--active');
 
         $($(this).attr('href')).addClass('tabs-content--active');
+        $('.product-slider').slick('setPosition');
     });
 
     $('.product-item_favorite').on('click', function () {
@@ -34,8 +35,8 @@ $(function()
     $('.product-slider').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
-        prevArrow: '<button class="product-slider_slider-btn product-slider_slider-btnprev"><img src="~/images/left-arrow.png" alt=""></button>',
-        nextArrow: '<button class="product-slider_slider-btn product-slider_slider-btnnext"><img src="~/images/right-arrow.png" alt=""></button>',
+        prevArrow: '<button class="product-slider_slider-btn product-slider_slider-btnprev"><img src="images/left-arrow.png" alt=""></button>',
+        nextArrow: '<button class="product-slider_slider-btn product-slider_slider-btnnext"><img src="images/right-arrow.png" alt=""></button>',
 
         responsive: [
             {
